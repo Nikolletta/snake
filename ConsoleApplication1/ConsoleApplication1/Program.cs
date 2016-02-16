33a0b4f537460ebd9576bf470e2a055444227ffd
Nikolletta <avnikka@yandex.ru>
2016-02-16 10:39:27 +0300

    Приветствие

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Snake_Game");
+            Console.ReadLine();
         }
     }
 }
