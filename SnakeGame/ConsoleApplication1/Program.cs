84f8995ac482f1ef788627904e1d83074d0c0cdc
Nikolletta <avnikka@yandex.ru>
2016-02-16 12:15:46 +0300

    Добавлен список точек

--- SnakeGame/ConsoleApplication1/Program.cs
+++ SnakeGame/ConsoleApplication1/Program.cs
@@ -19,6 +19,24 @@ namespace ConsoleApplication1
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            List<int> newlist = new List<int>();
+            newlist.Add(0);
+            newlist.Add(1);
+            newlist.Add(2);
+
+            int x = newlist[0];
+            int y = newlist[1];
+            int z = newlist[2];
+
+            foreach(int i in newlist)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<Point> pList = new List<Point>;
+            pList.Add(p1);
+            pList.Add(p2);
+
             Console.ReadLine();
         }
     }
